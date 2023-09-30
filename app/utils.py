af356b0ff59acdcc1b73672d2871db6a03fcb7cc
gumaonelove <bulat.gumerov@tattelecom.digital>
2023-10-01 00:20:35 +0300

    fix

--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,11 @@
-def get_file(file) -> dict:
+def get_file(file) -> str:
     try:
         contents = file.file.read()
         with open(file.filename, 'wb') as f:
             f.write(contents)
     except Exception:
-        return {"message": "There was an error uploading the video"}
+        return "There was an error uploading the video"
     finally:
         file.file.close()
 
-    return {'message': 'success'}
+    return 'success'
