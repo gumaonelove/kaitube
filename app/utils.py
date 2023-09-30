084859918daa82abb23322b8b59623a6c537ab23
gumaonelove <bulat.gumerov@tattelecom.digital>
2023-10-01 02:20:40 +0300

    fix

--- app/utils.py
+++ app/utils.py
@@ -1,15 +1,16 @@
 def get_file(file) -> str:
-    try:
-        contents = file.file.read()
-        with open('app/files/' + file.filename, 'wb') as f:
-            f.write(contents)
-    except Exception:
-        print('ERROR', Exception)
-        return "There was an error uploading the video"
-    finally:
-        file.file.close()
+    #try:
+    contents = file.file.read()
+    with open('app/files/' + file.filename, 'wb') as f:
+        f.write(contents)
+    # except Exception:
+    #     print('ERROR', Exception)
+    #     return "There was an error uploading the video"
+    # finally:
+    #     file.file.close()
 
     return 'success'
+
 
 def get_transcription(file_name: str) -> str:
     with open('app/files/' + file_name, 'r') as f:
