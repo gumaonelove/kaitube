547229d4c3e776dc63f1f78db5d371e8fc1c766b
gumaonelove <bulat.gumerov@tattelecom.digital>
2023-10-01 05:53:04 +0300

    fix

--- app/routers.py
+++ app/routers.py
@@ -22,10 +22,18 @@ async def listening(video: UploadFile = File(...), transcription: UploadFile = F
     prefix = "summarize: "
 
     transcription_text = get_transcription('app/files/' + transcription.filename)
-    summarization_transcription = get_bert_extractive_summarizer(transcription_text)['summary'][:1000]
+    summarization_transcription = get_bert_extractive_summarizer(transcription_text)
+    if summarization_transcription:
+        summarization_transcription = summarization_transcription['summary'][:1000]
+    else:
+        summarization_transcription = ' '
 
     video_text = video_to_text.predict('app/files/' + video.filename)
-    video_transcription = get_bert_extractive_summarizer(video_text)['summary'][:1000]
+    video_transcription = get_bert_extractive_summarizer(video_text)
+    if video_transcription:
+        video_transcription = video_transcription['summary'][:1000]
+    else:
+        video_transcription = ' '
 
     summarization = stt_summarizer.predict(prefix + summarization_transcription + '<tab>' + video_transcription)
 
